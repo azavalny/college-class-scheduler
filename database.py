2a44f7445cc57dc97be78dabdf8e2858bd9ed86e
Alex Zavalny <az548@drexel.edu>
2022-01-31 16:54:15 -0500

    added database and some basic usage functions to query from it

---
+++ database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect(name + ".db") 
+q = conn.cursor() #let's us execute queries
+
+
+def create_table(name):
+    """Initializes table of with a certain name"""
+    q.execute("""CREATE TABLE """ + name + """ (
+        
+    )""")
+    conn.commit()
+    conn.close()
+
+def insert_into(data):
+    """Insert data into database table"""
+    q.execute("INSERT INTO _____ VALUES ()")
+
+def get():
+    """Fetches a row from the database table"""
+    return q.fetchone()
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import fuzzy_finding
+import tms_scraper
+
 def main():
+    #insert into database the data for each class after scraped
     pass
+
+
+def getCourseInfo(course_title):
+    """Returns the course information from a given course title. If no matches are found, fuzzy finding is implemented to check if the user mispelled the title"""
+    pass
+
 
 if __name__ = "__main__":
     main()
