0110528cac4acdd46aca741d72308df5c9c427ba
Shivang Patel <30023891+shivangp76@users.noreply.github.com>
2022-02-03 14:17:33 -0500

    added reading csv

--- scripts/seed_tms_data.py
+++ scripts/seed_tms_data.py
@@ -1,21 +1,32 @@
 import sqlite3
+import csv
 
-conn = sqlite3.connect(name + ".db")
-q = conn.cursor()
+with open('../data/tms_data.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        # Split out data
+        subject_code = row[0]
+        course_number = row[1]
+        instruction_type = row[2]
+        instruction_method = row[3]
+        section = row[4]
+        crn_url = row[6]
+        crn = row[7]
+        course_title = row[8]
+        days = row[9]
+        times = row[10]
+        finals_day = row[11]
+        finals_time = row[12]
+        instructor = row[13]
 
+        # Create table
+        connection = sqlite3.connect("classes.db")
+        cursor = connection.cursor()
+        # TODO
+        cursor.execute(f"CREATE TABLE courses (name TEXT, species TEXT, tank_number INTEGER)")
 
-def create_table(name):
-    """Initializes table of with a certain name"""
-    q.execute("""CREATE TABLE """ + name + """ (
-
-    )""")
-    conn.commit()
-    conn.close()
-
-def insert_into(data):
-    """Insert data into database table"""
-    q.execute("INSERT INTO _____ VALUES ()")
-
-def get():
-    """Fetches a row from the database table"""
-    return q.fetchone()
+        # Insert table
+        # TODO
+        q.execute("INSERT INTO _____ VALUES ()")
+        conn.commit()
+        conn.close()
