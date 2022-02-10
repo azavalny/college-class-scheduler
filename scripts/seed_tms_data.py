7e5181bda6d1be2497d4613c0513c7408edccb26
Shivang Patel <30023891+shivangp76@users.noreply.github.com>
2022-02-10 13:41:40 -0500

    Fixed data seeding in db

--- scripts/seed_tms_data.py
+++ scripts/seed_tms_data.py
@@ -1,31 +1,33 @@
 import sqlite3
 import csv
 
+# Create table
+connection = sqlite3.connect("../data/courses.db")
+cursor = connection.cursor()
+cursor.execute(f"CREATE TABLE courses (subject_code TEXT, course_number TEXT, instruction_type TEXT, instruction_method TEXT, section TEXT, crn_url TEXT, crn TEXT, course_title TEXT, days TEXT, times TEXT, finals_day TEXT, finals_time TEXT, instructor TEXT)")
+
 with open('../data/winter-tms.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    spamreader = csv.reader(csvfile)
     for i, row in enumerate(spamreader):
         if (i == 0): continue
+        print(row)
         # Split out data
         subject_code = row[0]
         course_number = row[1]
         instruction_type = row[2]
         instruction_method = row[3]
         section = row[4]
-        crn_url = row[6]
-        crn = row[7]
-        course_title = row[8]
-        days = row[9]
-        times = row[10]
-        finals_day = row[11]
-        finals_time = row[12]
-        instructor = row[13]
-
-        # Create table
-        connection = sqlite3.connect("../data/courses.db")
-        cursor = connection.cursor()
-        cursor.execute(f"CREATE TABLE courses (subject_code TEXT, course_number TEXT, instruction_type TEXT, instruction_method TEXT, section TEXT, crn_url TEXT, crn TEXT, course_title TEXT, days TEXT, times TEXT, finals_day TEXT, finals_time TEXT, instructor TEXT)")
+        crn_url = row[5]
+        crn = row[6]
+        course_title = row[7]
+        days = row[8]
+        times = row[9]
+        finals_day = row[10]
+        finals_time = row[11]
+        instructor = row[12]
 
         # Insert table
-        cursor.execute(f"INSERT INTO courses VALUES ('{subject_code}', '{course_number}', '{instruction_type}', '{instruction_method}', '{section}', '{crn_url}', '{crn}', '{course_title}', '{days}', '{times}', '{finals_day}', '{finals_time}', '{instructor}')")
-        connection.commit()
-        connection.close()
+        cursor.execute(f'INSERT INTO courses VALUES ("{subject_code}", "{course_number}", "{instruction_type}", "{instruction_method}", "{section}", "{crn_url}", "{crn}", "{course_title}", "{days}", "{times}", "{finals_day}", "{finals_time}", "{instructor}")')
+
+connection.commit()
+connection.close()
