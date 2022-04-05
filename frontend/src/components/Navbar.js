839bc52e89191ae131d55c72a2bf3e5c1ed0a4ce
Shivang Patel <30023891+shivangp76@users.noreply.github.com>
2022-04-05 16:35:19 -0400

    Removed extra states in the Navbarg

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
-  const [show, setShow] = useState(false);
-  const [product, setProduct] = useState(false);
-  const [deliverables, setDeliverables] = useState(false);
-  const [profile, setProfile] = useState(false);
   return (
     <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
       <div className="mb-2 sm:mb-0">
