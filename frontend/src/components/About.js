ee9d72e447af2e714f17350ff2a8369497127eab
Shivang Patel <30023891+shivangp76@users.noreply.github.com>
2022-05-19 14:59:47 -0400

    Fixed issue with headshots not loading on about page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -35,24 +35,21 @@ const people = [
   {
     name: 'Alex Zavalny',
     email: 'az548@drexel.edu',
-    image:
-      'https://media-exp1.licdn.com/dms/image/C4E03AQGv7JrjtI5wpA/profile-displayphoto-shrink_800_800/0/1649010036102?e=1654732800&v=beta&t=JwtUUBqsu7_NwgF-s-0mJwAoP2vAiu627BwtU7NMzgo',
+    image: './images/headshots/alex.jpg',
   },
   {
     name: 'Shivang Patel',
     email: 'sp3699@drexel.edu',
-    image:
-        'https://media-exp1.licdn.com/dms/image/C4D03AQFYyUGjbLkA-A/profile-displayphoto-shrink_800_800/0/1640570806299?e=1654732800&v=beta&t=aqls6yzJjE2YqjjPJKkZ9r0_TDNrUAjKZTMGhBj_Apo',
+    image: './images/headshots/shivang.png',
   },
   {
     name: 'Ryen Ling',
     email: 'rl826@drexel.edu',
-    image:
-      'https://media-exp1.licdn.com/dms/image/C4E03AQFJXjRAQD-35A/profile-displayphoto-shrink_800_800/0/1625248068593?e=1654732800&v=beta&t=WB2zr7lzT3yDReguoIORA791aVeCXw2oZ4SNtsqpoYQ',
+    image: './images/headshots/ryen.jpg',
   },
   {
     name: 'Siddarth Kampalli',
     email: 'sk3999@drexel.edu',
-    image: 'https://media-exp1.licdn.com/dms/image/C4D03AQH7yDhZpTDjNQ/profile-displayphoto-shrink_400_400/0/1594709657208?e=1654732800&v=beta&t=wJ72jZTLfY0C8cuzb9oKTJj3G5TypgIO3QYFeIlC2Bg',
+    image: './images/headshots/sid.jpg',
   },
 ]
